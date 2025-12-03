76be3a2d576050bb3dfd6a4ffa522148854c91fb
REFORMAT3R <dmamanihuayh@unsa.edu.pe>
2025-12-03 12:04:24 -0500

    Arreglando Ejercicio02

--- Ejercicio02.py
+++ Ejercicio02.py
@@ -1,43 +1,62 @@
-ingreso_mensual=float(input("Ingreso mensual: "))
-ingreso_anual=ingreso_mensual*14
+while True:
+    ingreso_mensual=float(input("Ingrese ingreso mensual (0 para salir): "))
+    if ingreso_mensual==0:
+        print("Programa finalizado.")
+        break
 
-# Tramos
-lim1=20000
-lim2=50000
-lim3=100000
+    # 12 sueldos + 2 aguinaldos
+    ingreso_anual=ingreso_mensual*14
+    restante=ingreso_anual
 
-imp1=imp2=imp3=imp4=0
-restante=ingreso_anual
+    # Tramo 1: [0 – 20000] a 0%
+    if restante > 20000:
+        tramo1 = 20000
+    else:
+        tramo1 = restante
+    imp1 = tramo1 * 0
+    restante -= tramo1
 
-# Tramo 1: [0 - 20000], 0%
-if restante>0:
-    tramo=min(restante,lim1)
-    imp1=tramo*0
-    restante-=tramo
+    # Tramo 2: (20000 – 50000] a 10%
+    if restante > 0:
+        if restante > 30000:    # 50000 - 20000
+            tramo2 = 30000
+        else:
+            tramo2 = restante
+        imp2 = tramo2 * 0.10
+        restante -= tramo2
+    else:
+        tramo2 = 0
+        imp2 = 0
 
-# Tramo 2: (20000 - 50000], 10%
-if restante>0:
-    tramo=min(restante,lim2-lim1)
-    imp2=tramo*0.10
-    restante-=tramo
+    # Tramo 3: (50000 – 100000] a 20%
+    if restante > 0:
+        if restante > 50000:    # 100000 - 50000
+            tramo3 = 50000
+        else:
+            tramo3 = restante
+        imp3 = tramo3 * 0.20
+        restante -= tramo3
+    else:
+        tramo3 = 0
+        imp3 = 0
 
-# Tramo 3: (50000 - 100000], 20%
-if restante>0:
-    tramo=min(restante,lim3-lim2)
-    imp3=tramo*0.20
-    restante-=tramo
+    # Tramo 4: >100000 a 30%
+    if restante > 0:
+        tramo4 = restante
+        imp4 = tramo4 * 0.30
+    else:
+        tramo4 = 0
+        imp4 = 0
 
-# Tramo 4: >100000, 30%
-if restante>0:
-    imp4=restante*0.30
+    total_impuesto = imp1 + imp2 + imp3 + imp4
+    tasa_efectiva = (total_impuesto / ingreso_anual) * 100
 
-total_imp=imp1+imp2+imp3+imp4
-tasa_efectiva=(total_imp/ingreso_anual)*100
-
-print("Ingreso anual:",ingreso_anual)
-print("Impuesto tramo 1:",imp1)
-print("Impuesto tramo 2:",imp2)
-print("Impuesto tramo 3:",imp3)
-print("Impuesto tramo 4:",imp4)
-print("Total impuesto:",total_imp)
-print("Tasa efectiva real:",tasa_efectiva,"%")
+    print("\n--- RESULTADOS ---")
+    print("Ingreso anual:", ingreso_anual)
+    print("Impuesto tramo 1 (0%):", imp1)
+    print("Impuesto tramo 2 (10%):", imp2)
+    print("Impuesto tramo 3 (20%):", imp3)
+    print("Impuesto tramo 4 (30%):", imp4)
+    print("TOTAL impuesto:", total_impuesto)
+    print("Tasa efectiva (%):", tasa_efectiva)
+    print("------------------\n")
